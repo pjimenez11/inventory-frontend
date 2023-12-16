70a3793c697e5f01c89f16ac7f7f68439197a443
pjimenez11 <pjimenez1025@uta.edu.ec>
2023-12-16 14:34:38 -0500

    Update styles and components

---
+++ src/components/laboratories/Laboratories.jsx
@@ -0,0 +1,12 @@
+export function Laboratories() {
+  return (
+    <div>
+      <h1 className="text-white">Laboratorios</h1>
+      <div className="flex gap-6 mt-3">
+        <div className="w-40 h-40 bg-oxford-blue-900 rounded-lg p-5">
+          <h2 className="text-white">Laboratorio 1</h2> </div>
+        <div className="w-72 h-40 bg-oxford-blue-900 rounded-lg"> </div>
+      </div>
+    </div>
+  );
+}
--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,8 +1,41 @@
+import { TERipple } from "tw-elements-react";
 import Dropdown from "./Dropdown";
 export default function Header() {
   return (
-    <header className="fixed md:top-2 md:rounded-md left-0 md:left-[270px] right-0 md:right-2 z-50 h-16 bg-gray-500  transition-all">
-      <div className="flex items-center justify-center h-full w-full">
+    <header className="fixed flex items-center  justify-between left-0 md:left-64 right-0  z-50 h-16 bg-oxford-blue-900  transition-all">
+      <div className="ml-6 md:w-80">
+        <div className="relative flex w-full flex-wrap items-stretch">
+          <input
+            type="text"
+            className="relative pr-9 h-10 m-0 -mr-0.5 block w-[1px] min-w-0 flex-auto rounded-lg border-solid
+            bg-oxford-blue-950  bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6]
+            text-neutral-400 outline-none transition duration-200 ease-in-out focus:z-[3]
+            focus:border-blue-500 focus:text-neutral-200 focus:outline-none"
+            placeholder="Search"
+            aria-label="Search"
+            aria-describedby="button-addon1"
+          />
+          <button
+            className="right-7 relative z-50 text-neutral-400 hover:text-neutral-200 focus:text-neutral-200 focus:outline-none"
+            type="button"
+            id="button-addon1"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              className="h-5 w-5"
+            >
+              <path
+                fillRule="evenodd"
+                d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </button>
+        </div>
+      </div>
+      <div className="">
         <Dropdown />
       </div>
     </header>
