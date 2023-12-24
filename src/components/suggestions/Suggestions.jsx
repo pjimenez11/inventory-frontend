f57e2f514f51e2b39e73eb8ddcf19ba0d62485cb
vKail <ajurado5411@uta.edu.ec>
2023-12-23 22:34:21 -0500

    Changes in the form and the position on himself

--- src/components/suggestions/Suggestions.jsx
+++ src/components/suggestions/Suggestions.jsx
@@ -12,7 +12,10 @@ const Suggestions = () => {
 
 
     return (
-        <div className="flex w-full text-oxford-blue-50">
+        <div className="flex flex-col w-full text-oxford-blue-50">
+            <div className="w-full">
+                <h1>Sugerencias anteriores</h1>
+            </div>
            <div className="w-full">
                 <div className="flex flex-row justify-between items-center "> 
                     <h1 className="flex text-oxford-blue-50 text-2xl">Sugerencias</h1>
@@ -20,27 +23,27 @@ const Suggestions = () => {
                 </div>
                {
                 showSuggestions === true && 
-                <div className="flex flex-col items-center w-full">
-                    <form className="flex flex-col w-full"> 
-                        <div class="grid grid-cols-1 gap-6 mt-4 ">
+                <div className="flex flex-col  items-center w-full">
+                    <form className="flex flex-col w-full px-6"> 
+                        <div className="grid grid-cols-1 gap-6 mt-4 ">
                             <div>
-                                <label className="text-blue-300 dark:text-gray-200 text-lg" for="username">Producto</label>
-                                <input id="product" type="text" class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"/>
+                                <label className="text-blue-300 dark:text-gray-200 text-lg" htmlFor="product">Producto</label>
+                                <input id="product" type="text" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"/>
                             </div>
 
                             <div>
-                                <label className="text-blue-300 dark:text-gray-200 text-lg" for="emailAddress">Laboratorio</label>
-                                <input id="emailAddress" type="email" class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"/>
+                                <label className="text-blue-300 dark:text-gray-200 text-lg" htmlFor="laboratory">Laboratorio</label>
+                                <input id="emailAddress" type="email" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"/>
                             </div>
 
                             <div>
-                                <label className="text-blue-300 dark:text-gray-200 text-lg" for="password">Custodio</label>
-                                <input id="custodian" type="password" class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"/>
+                                <label className="text-blue-300 dark:text-gray-200 text-lg" htmlFor="custodian">Custodio</label>
+                                <input id="custodian" type="password" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"/>
                             </div>
 
                             <div>
-                                <label className="text-blue-300 dark:text-gray-200 text-lg" for="passwordConfirmation">Descripción</label>
-                                <input id="laboratory" type="password" class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"/>
+                                <label className="text-blue-300 dark:text-gray-200 text-lg" htmlFor="description">Descripción</label>
+                                <textarea name="" id="" cols="30" rows="10" className="w-full rounded-sm"></textarea>
                             </div>
                         </div>
 
