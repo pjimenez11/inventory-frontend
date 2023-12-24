b152f4df19e209f222a83bbcf4d3903a8d8603e9
vKail <ajurado5411@uta.edu.ec>
2023-12-23 22:54:02 -0500

    Create cardSuggestions and change some colors

--- src/components/suggestions/Suggestions.jsx
+++ src/components/suggestions/Suggestions.jsx
@@ -18,7 +18,7 @@ const Suggestions = () => {
             </div>
            <div className="w-full">
                 <div className="flex flex-row justify-between items-center "> 
-                    <h1 className="flex text-oxford-blue-50 text-2xl">Sugerencias</h1>
+                    <h1 className="flex text-oxford-blue-50 text-xl">Sugerencias</h1>
                     <button className="px-6 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-green-400 rounded-lg hover:bg-green-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80" onClick={handleShowSuggestions}>Nueva sugerencia</button>
                 </div>
                {
@@ -27,22 +27,22 @@ const Suggestions = () => {
                     <form className="flex flex-col w-full px-6"> 
                         <div className="grid grid-cols-1 gap-6 mt-4 ">
                             <div>
-                                <label className="text-blue-300 dark:text-gray-200 text-lg" htmlFor="product">Producto</label>
+                                <label className="text-white dark:text-gray-200 text-lg" htmlFor="product">Producto</label>
                                 <input id="product" type="text" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"/>
                             </div>
 
                             <div>
-                                <label className="text-blue-300 dark:text-gray-200 text-lg" htmlFor="laboratory">Laboratorio</label>
+                                <label className="text-white dark:text-gray-200 text-lg" htmlFor="laboratory">Laboratorio</label>
                                 <input id="emailAddress" type="email" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"/>
                             </div>
 
                             <div>
-                                <label className="text-blue-300 dark:text-gray-200 text-lg" htmlFor="custodian">Custodio</label>
+                                <label className="text-white dark:text-gray-200 text-lg" htmlFor="custodian">Custodio</label>
                                 <input id="custodian" type="password" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"/>
                             </div>
 
                             <div>
-                                <label className="text-blue-300 dark:text-gray-200 text-lg" htmlFor="description">Descripción</label>
+                                <label className="text-white dark:text-gray-200 text-lg" htmlFor="description">Descripción</label>
                                 <textarea name="" id="" cols="30" rows="10" className="w-full rounded-sm"></textarea>
                             </div>
                         </div>
