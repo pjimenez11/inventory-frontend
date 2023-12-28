46dbc12ff2aebf2a26208af70ea3e0f5987cf795
scaminom <jcamino7758@uta.edu.ec>
2023-12-27 20:39:07 -0500

    Add assets form

---
+++ src/components/assets/Assets.jsx
@@ -0,0 +1,10 @@
+import { IoIosAddCircleOutline } from "react-icons/io";
+import FormAsset from "./FormAsset";
+
+export function Assets() {
+  return (
+    <section className="min-h-max flex flex-col gap-6 items-center pb-6">
+      <FormAsset />;
+    </section>
+  );
+}
